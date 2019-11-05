74ad98f62806899ec0e380aea3b47716072870f2
rca_matheus_rambo <matheus.rambo@operacao.rcadigital.com.br>
2019-11-05 13:54:12 -0300

    added support to store text files into an image

--- behind-image.py
+++ behind-image.py
@@ -4,6 +4,18 @@ from source.r_file import get_file_extension, is_an_image
 
 args_length = len(sys.argv)
 
+def open_file_if_exists(file_path:str):
+    try:
+        return open(file_path, "rt")
+    except Exception:
+        return None
+
+def read_file_content(file_path:str):
+    file = open_file_if_exists(file_path)
+    if file is not None:
+        return file.read()
+    return file_path
+
 def stenography(original_file_path:str, secret_message:str):
     extension = get_file_extension(original_file_path)
     if is_an_image(extension):
@@ -21,9 +33,11 @@ def stenography(original_file_path:str, secret_message:str):
         print("The extension: {} is not in an image format!".format(extension))    
 
 if args_length == 2:
-    stenography(sys.argv[1], input("Insert the secret message to hide: "))
+    content = read_file_content(input("Insert the secret message to hide: "))
+    stenography(sys.argv[1], content)
 elif args_length >= 3:
-    stenography(sys.argv[1], sys.argv[2])
+    content = read_file_content(sys.argv[2])
+    stenography(sys.argv[1], content)
 else:
-    stenography(input("Insert the name of the file to copy and hide the information: "), input("Insert the secret message to hide: "))
+    stenography(input("Insert the name of the file to copy and hide the information: "), read_file_content(input("Insert the secret message to hide: ")))
 
