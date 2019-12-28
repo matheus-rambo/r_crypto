1433446552ae7f54ff880864ed45deb1facb03da
matheusrambo <matheusrambo@protonmail.ch>
2019-12-28 20:51:15 -0300

    load config from json file

--- source/mail.py
+++ source/mail.py
@@ -1,17 +1,18 @@
-import ssl
+from ssl import create_default_context
 from smtplib import SMTP
+from json import loads
+from app_util import read_file_content
 
-smtp_server = 'smtp.gmail.com'
-port = 587
-sender_email = ""
-password = ""
-receiver_email = ""
+config = read_file_content(file_name = '../config/mail-config.json', buffer_size = 2048)
+smtp_oject = loads(config)['smtp']
+email_config = loads(config)['mail_config']
+
 message = """\
-    Subject: Teste envio de e-mail
+    Subject: Teste envio de e-mail 2
 
-    Ola."""
+    Hello Friend"""
     
-server = SMTP(smtp_server, port)
-server.starttls(context = ssl.create_default_context())
-server.login(sender_email, password)
-server.sendmail(sender_email, receiver_email, message)
+server = SMTP(smtp_oject['server'], smtp_oject['port'])
+server.starttls(context = create_default_context())
+server.login(email_config['e-mail'], email_config['password'])
+server.sendmail(email_config['e-mail'], '', message)
